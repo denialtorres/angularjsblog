3cb45c5085a01ccd457e84ec473d41166bddce9a
Daniel Torres <denial.torres@gmail.com>
2017-07-11 22:36:21 -0500

    correct comments of facebook

--- app/showArticle/showArticle.js
+++ app/showArticle/showArticle.js
@@ -7,32 +7,54 @@ angular.module('myApp.showArticle', ['ngRoute'])
   });
 }])
 
-.directive('fbCommentBox', function() {
-  function createHTML(href, numposts, colorscheme, width) {
-    return '<div class="fb-comments" ' +
-      'data-href="' + href + '" ' +
-      'data-numposts="' + numposts + '" ' +
-      'data-colorsheme="' + colorscheme + '" ' +
-      'data-width="' + width + '">' +
-      '</div>';
-  }
 
+.directive('facebookComments', ['$location', function ($location) {
   return {
-    restrict: 'A',
-    scope: {},
-    link: function postLink(scope, elem, attrs) {
-      attrs.$observe('pageHref', function(newValue) {
-        var href = newValue;
-        var numposts = attrs.numposts || 5;
-        var colorscheme = attrs.colorscheme || 'light';
-        var width = attrs.width || '100%';
-        elem.html(createHTML(href, numposts, colorscheme, width));
-        FB.XFBML.parse(elem[0]);
-      });
-    }
-  };
-})
+      restrict: 'E',
+      templateUrl: "scripts/directives/facebookComments.html",
+      scope:{},
+      replace: true,
+      link: function(scope, el, attr){
+        scope.curPath = $location.absUrl();
+      }
+   };
+}])
 
+
+.directive('dynFbCommentBox',['$timeout', function ($timeout) {
+    function createHTML(href, numposts, colorscheme, width) {
+        return '<div class="fb-comments" ' +
+                       'data-href="' + href + '" ' +
+                       'data-numposts="' + numposts + '" ' +
+                       'data-colorsheme="' + colorscheme + '" ' +
+                       'data-width="' + width + '">' +
+               '</div>';
+    }
+
+    return {
+        restrict: 'A',
+        scope: {},
+        link: function postLink(scope, elem, attrs) {
+          //
+          // Use timeout in order to be called after all watches are done and FB script is loaded
+          //
+          attrs.$observe('pageHref', function (newValue) {
+              var href        = newValue;
+              var numposts    = attrs.numposts    || 5;
+              var colorscheme = attrs.colorscheme || 'light';
+              var width = attrs.width || '100%';
+              elem.html(createHTML(href, numposts, colorscheme, width));
+              $timeout(function () {
+                if (typeof FB != 'undefined'){
+                    FB.XFBML.parse(elem[0]);
+                  }
+              });
+          });
+
+          
+        }
+    };
+}])
 
 .controller('ShowArticleCtrl', ['$scope', '$routeParams','$firebaseArray', '$firebase', '$location', function($scope, $routeParams, $firebaseArray, $firebase, $location){
   console.log('Estas en Show Article Controller')
